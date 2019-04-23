e680670e8360596a94562426c2d93af3931d8222
Madeline Carr <madelinencarr@Gmail.com>
2019-04-23 12:27:10 -0400

    CSS Minify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
 var reload = browserSync.reload;
+var cleanCSS = require("gulp-clean-css");
+let sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("sass", function() {
   return gulp
@@ -25,7 +27,19 @@ gulp.task("browser-sync", function() {
   });
 });
 
+gulp.task("sass:minify", function() {
+  return gulp
+    .src("./public/css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./public/css"));
+  // .pipe(browserSync.stream());
+});
+
 //Runs automatically after gulp command in terminal
 gulp.task("default", ["sass", "browser-sync"], function() {
   gulp.watch("./src/scss/**/*.scss", ["sass"]);
 });
+
+gulp.task("production", ["sass:minify"]);
