fcb7baa9828a867dec8186b7d534bbcdd8aac500
Madeline Carr <madelinencarr@Gmail.com>
2019-04-22 22:14:49 -0400

    First Gulp Task Examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
 var gulp = require("gulp");
+
+gulp.task("printName", function() {
+  console.log("My name is Maddie!");
+});
+
+gulp.task("printAge", function() {
+  console.log("I am 22!");
+});
+
+gulp.task("default", ["printName", "printAge"]);
