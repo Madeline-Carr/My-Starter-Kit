62cbe6743004f80631a945b6818c6918fe84f7dc
Madeline Carr <madelinencarr@Gmail.com>
2019-04-22 23:07:40 -0400

    Auto Vendor Prefixers

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function() {
   return gulp
     .src("./src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
     .pipe(gulp.dest("./public/css"));
 });
 
