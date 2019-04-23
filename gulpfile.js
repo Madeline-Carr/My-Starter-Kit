3937b527ec515a1ba7ac5847b094a0013d2dfe42
Madeline Carr <madelinencarr@Gmail.com>
2019-04-23 00:01:34 -0400

    Automatic Reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require("browser-sync").create();
+var reload = browserSync.reload;
 
 gulp.task("sass", function() {
   return gulp
@@ -11,10 +13,19 @@ gulp.task("sass", function() {
         browsers: ["last 2 versions"]
       })
     )
-    .pipe(gulp.dest("./public/css"));
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: "./public",
+    notify: false,
+    open: false
+  });
 });
 
 //Runs automatically after gulp command in terminal
-gulp.task("default", ["sass"], function() {
+gulp.task("default", ["sass", "browser-sync"], function() {
   gulp.watch("./src/scss/**/*.scss", ["sass"]);
 });
