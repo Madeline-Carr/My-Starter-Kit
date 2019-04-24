5acdc14adc4f4b53d3663c9a543bd4051136c820
Madeline Carr <madelinencarr@Gmail.com>
2019-04-23 23:11:20 -0400

    React File Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello World from React boilerplate</h1>;
-  }
-}
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+
+const title = 'Rada';
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js"
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  }
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
 };
